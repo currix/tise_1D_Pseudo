48e7d5fbf66fc218d6f416fc290975f026a1d55e
Currix <currix@server.fake>
2014-02-09 03:09:28 +0100

    Remove typos and change from analytical to numerical the reported calculations

--- src/e1_isqw.f90
+++ src/e1_isqw.f90
@@ -1,6 +1,6 @@
 SUBROUTINE E1_ISQW(Iprint, I_toten)
   !
-  ! <GS|X|AVEC(i)>
+  ! < bound | X | AVEC(i) >
   !
   !
   USE constants
@@ -12,9 +12,9 @@ SUBROUTINE E1_ISQW(Iprint, I_toten)
   INTEGER(KIND = I4B), INTENT(IN) :: Iprint, I_toten
   !
   INTEGER(KIND = I4B) :: Ierr, Ifail, i, k, j, i_state
-  REAL(KIND = DP) :: error, E1_numerical, aj, ak
+  REAL(KIND = DP) :: error, E1_analytical, aj, ak
   REAL(KIND = DP), DIMENSION(:), ALLOCATABLE :: matrix_element, Total_E1
-  REAL(KIND = DP), DIMENSION(:,:), ALLOCATABLE ::  matrix_x,  E1_analytical
+  REAL(KIND = DP), DIMENSION(:,:), ALLOCATABLE ::  matrix_x,  E1_numerical
   !
   CHARACTER(LEN=65) :: filename_TM, filename_E1
   CHARACTER(LEN=65) :: file = 'E1'
@@ -40,11 +40,10 @@ SUBROUTINE E1_ISQW(Iprint, I_toten)
      STOP
   ENDIF
   !
-  Total_E1 = 0.0_DP
   !
-  ALLOCATE(E1_analytical(1:dim_BOX,1:I_toten), STAT = Ierr)
+  ALLOCATE(E1_numerical(1:dim_BOX,1:I_toten), STAT = Ierr)
   IF (Ierr /= 0) THEN
-     WRITE(*,*), "E1_analytical  allocation request denied."
+     WRITE(*,*), "E1_numerical  allocation request denied."
      STOP
   ENDIF
   !
@@ -70,22 +69,6 @@ SUBROUTINE E1_ISQW(Iprint, I_toten)
      !
      WRITE(*,*), " ", i_state,"-th state :: Numerical method"
      !
-     DO i = 1, DIM_BOX
-        !     
-        matrix_element = Avec_box_x(:,i_state)*X_grid(:)*Avec_box_x(:,i)  
-        !
-        E1_numerical = 0.0_DP
-        Ifail = 0
-        !
-        CALL D01GAF(X_Grid, matrix_element, dim_X, E1_numerical, error, Ifail)
-        !
-     WRITE(*,15), i, "-th state energy: ", Aval_box(i), " <",i_state,"| X |Avec(",i,")> = ", E1_numerical
-     ENDDO
-     !
-     !
-     WRITE(*,*), "Analytical method"
-     !
-     !
      WRITE(filename_E1, '(A, "_",A,"_N",I2,"_",I1,".dat")') TRIM(prog), TRIM(file), dim_BOX, i_state
      IF ( dim_BOX < 10) THEN !to avoid spaces
         WRITE(filename_E1, '(A, "_",A,"_N",I1,"_",I1,".dat")') TRIM(prog), TRIM(file), dim_BOX, i_state
@@ -96,24 +79,40 @@ SUBROUTINE E1_ISQW(Iprint, I_toten)
      OPEN(UNIT = 78, FILE = filename_E1, STATUS = "UNKNOWN", ACTION = "WRITE")
      !
      WRITE(78,*) "# BOX  dim_BOX = ", dim_BOX, " Box radius = ", X_max, " fm"
-     WRITE(78,*) "#Aval_box(i)    E1_analytical**2"
-     !
-     !
+     WRITE(78,*) "#Aval_box(i)    E1_numerical**2"
      !
      DO i = 1, DIM_BOX
-        E1_analytical(i, i_state) = DOT_PRODUCT(Avec_box(:,i_state),MATMUL(matrix_x,Avec_box(:,i)))
+        !     
+        matrix_element = Avec_box_x(:,i_state)*X_grid(:)*Avec_box_x(:,i)  
         !
-        WRITE(*,15), i, "-th state energy: ", Aval_box(i), " <",i_state,"| X |Avec(",i,")> = ", E1_analytical(i, i_state)
+        E1_numerical(i, i_state) = 0.0_DP
+        Ifail = 0
+        !
+        CALL D01GAF(X_Grid, matrix_element, dim_X, E1_numerical(i, i_state), error, Ifail)
+        !
+        WRITE(*,15), i, "-th state energy: ", Aval_box(i), " <",i_state,"| X |Avec(",i,")> = ", E1_numerical(i, i_state)
+        !
+        Total_E1(i_state) = Total_E1(i_state) + (E1_numerical(i, i_state)**2)
+        !
         ! SAVING E1
-        WRITE(78,11)  Aval_box(i), E1_analytical(i, i_state)**2
-        !
-        Total_E1(i_state) = Total_E1(i_state) + (E1_analytical(i, i_state)**2)
+        WRITE(78,11)  Aval_box(i), E1_numerical(i, i_state)**2
         !
      ENDDO
      !
      CLOSE(UNIT = 78)
      !
      WRITE(*,*), "Total E1: ", i_state, Total_E1(i_state)
+     !
+     !
+     WRITE(*,*), "Analytical method"
+     !
+     DO i = 1, DIM_BOX
+        E1_analytical = DOT_PRODUCT(Avec_box(:,i_state),MATMUL(matrix_x,Avec_box(:,i)))
+        !
+        WRITE(*,15), i, "-th state energy: ", Aval_box(i), " <",i_state,"| X |Avec(",i,")> = ", E1_analytical
+        !
+     ENDDO
+     !
      !
   ENDDO
   !
@@ -127,11 +126,11 @@ SUBROUTINE E1_ISQW(Iprint, I_toten)
   OPEN(UNIT = 76, FILE = filename_TM, STATUS = "UNKNOWN", ACTION = "WRITE")
   !
   WRITE(76,*) "# BOX  dim_BOX = ", dim_BOX, " Box radius = ", X_max, " fm"
-  WRITE(76,*) "#Aval_box(i)    E1_analytical"
+  WRITE(76,*) "#Aval_box(i)    E1_numerical"
   !
   DO i = 1, dim_BOX     !
      ! SAVING E1
-     WRITE(76,12)  Aval_box(i), E1_analytical(i,1:I_toten)
+     WRITE(76,12)  Aval_box(i), E1_numerical(i,1:I_toten)
      !
   ENDDO
   !
@@ -162,9 +161,9 @@ SUBROUTINE E1_ISQW(Iprint, I_toten)
      STOP
   ENDIF
   !
-  DEALLOCATE(E1_analytical, STAT = Ierr)
+  DEALLOCATE(E1_numerical, STAT = Ierr)
   IF (Ierr /= 0) THEN
-     WRITE(*,*), "E1_analytical deallocation request denied."
+     WRITE(*,*), "E1_numerical deallocation request denied."
      STOP
   ENDIF
   !
--- src/e2_isqw.f90
+++ src/e2_isqw.f90
@@ -1,8 +1,7 @@
 SUBROUTINE E2_ISQW(Iprint, I_toten)
   !
-  ! <GS|X**2|AVEC(i)>
+  ! < bound | X**2 | AVEC(i) >
   !
-  ! $Id: e2_isqw.f90,v 1.6 2013/10/05 17:56:24 curro Exp curro $
   !
   USE constants
   USE nrtype
@@ -13,9 +12,9 @@ SUBROUTINE E2_ISQW(Iprint, I_toten)
   INTEGER(KIND = I4B), INTENT(IN) :: Iprint, I_toten
   !
   INTEGER(KIND = I4B) :: Ierr, Ifail, i, k, j, i_state
-  REAL(KIND = DP) :: error, E2_numerical
+  REAL(KIND = DP) :: error, E2_analytical
   REAL(KIND = DP), DIMENSION(:), ALLOCATABLE :: matrix_element, Total_E2
-  REAL(KIND = DP), DIMENSION(:,:), ALLOCATABLE ::  matrix_x2,  E2_analytical
+  REAL(KIND = DP), DIMENSION(:,:), ALLOCATABLE ::  matrix_x2,  E2_numerical
   !
   CHARACTER(LEN=65) :: filename_E2, filename_TM
   CHARACTER(LEN=65) :: file = 'E2'
@@ -42,17 +41,18 @@ SUBROUTINE E2_ISQW(Iprint, I_toten)
      STOP
   ENDIF
   !
-  Total_E2 = 0.0_DP
   !
-  ALLOCATE(E2_analytical(1:dim_BOX,1:I_toten), STAT = Ierr)
+  ALLOCATE(E2_numerical(1:dim_BOX,1:I_toten), STAT = Ierr)
   IF (Ierr /= 0) THEN
-     WRITE(*,*), "E2_analytical  allocation request denied."
+     WRITE(*,*), "E2_numerical  allocation request denied."
      STOP
   ENDIF
   !
   !
+  Total_E2 = 0.0_DP
   ! Compute X**2 matrix in the ISQW basis
   matrix_x2 = 0.0_DP
+  !
   DO j = 1, DIM_BOX
      DO k = 1, DIM_BOX  ! can make just half matrix and consider it symmetric!!!
         ! Equal parity case
@@ -95,51 +95,57 @@ SUBROUTINE E2_ISQW(Iprint, I_toten)
   !
   matrix_x2 = matrix_x2*(X_max**2)
   !
+  ! Numerical method
   DO i_state = 1, I_toten
      !
      WRITE(*,*), " ", i_state,"-th state :: Numerical method"
+     !
+     !
+     WRITE(filename_E2, '(A, "_",A,"_N",I2,"_",I1,".dat")') TRIM(prog), TRIM(file), dim_BOX, i_state
+     IF ( dim_BOX < 10) THEN !to avoid spaces
+        WRITE(filename_E2, '(A, "_",A,"_N",I1,"_",I1,".dat")') TRIM(prog), TRIM(file), dim_BOX, i_state
+     ELSE IF ( dim_BOX > 99) THEN 
+        WRITE(filename_E2, '(A, "_",A,"_N",I3,"_",I1,".dat")') TRIM(prog), TRIM(file), dim_BOX, i_state
+     ENDIF
+     !
+     !
+     OPEN(UNIT = 78, FILE = filename_E2, STATUS = "UNKNOWN", ACTION = "WRITE")
+     !
+     WRITE(78,*) "# BOX  dim_BOX = ", dim_BOX, " Box radius = ", X_max, " fm"
+     WRITE(78,*) "#Aval_box(i)    E2_numerical**2"
      !
      DO i = 1, DIM_BOX
         !     
         matrix_element = Avec_box_x(:,i_state)*X_grid(:)*X_grid(:)*Avec_box_x(:,i)  
         !
-        E2_numerical = 0.0_DP
+        E2_numerical(i, i_state) = 0.0_DP
         Ifail = 0
         !
-        CALL D01GAF(X_Grid, matrix_element, dim_X, E2_numerical, error, Ifail)
+        CALL D01GAF(X_Grid, matrix_element, dim_X, E2_numerical(i, i_state), error, Ifail)
         !
-        WRITE(*,15), i, "-th state energy: ", Aval_box(i), " <GS | X^2 |Avec(",i,")> = ", E2_numerical
+        WRITE(*,15), i, "-th state energy: ", Aval_box(i), " <",i_state,"| X^2 |Avec(",i,")> = ", E2_numerical(i, i_state)
+        !
+        Total_E2(i_state) = Total_E2(i_state) + (E2_numerical(i, i_state)**2)
+        !
+        ! SAVING E2
+        WRITE(78,11)  Aval_box(i), E2_numerical(i, i_state)**2
+        !
      ENDDO
+     !
+     !
+     CLOSE(UNIT = 78)
+     !
+     WRITE(*,*), "Total E2: ", i_state, Total_E2(i_state)
      !
      !
      WRITE(*,*), "Analytical method"
      !
-     Total_E2 = 0.0_DP
-     !
-     WRITE(filename_E2, '(A, "_",A,"_N",I2,"_",I1, ".dat")') TRIM(prog), TRIM(file), dim_BOX, i_state
-     IF ( dim_BOX < 10) THEN !to avoid spaces
-        WRITE(filename_E2, '(A, "_",A,"_N",I1,"_",I1, ".dat")') TRIM(prog), TRIM(file), dim_BOX, i_state
-     ELSE IF ( dim_BOX > 99) THEN 
-        WRITE(filename_E2, '(A, "_",A,"_N",I3,"_",I1, ".dat")') TRIM(prog), TRIM(file), dim_BOX, i_state
-     ENDIF
-     !
-     OPEN(UNIT = 79, FILE = filename_E2, STATUS = "UNKNOWN", ACTION = "WRITE")
-     !
-     WRITE(79,*) "# BOX  dim_BOX = ", dim_BOX, " Box radius = ", X_max, " fm"
-     WRITE(79,*) "# Aval_box(i)     E2_analytical**2"
-     !
      DO i = 1, DIM_BOX
-        E2_analytical(i, i_state) = DOT_PRODUCT(Avec_box(:,i_state), MATMUL(matrix_x2,Avec_box(:,i)))
-        WRITE(*,15), i, "-th state energy: ", Aval_box(i), " <bnd | X^2 |Avec(",i,")> = ",  E2_analytical(i, i_state)
-        ! SAVING E2
-        WRITE(79,11)  Aval_box(i), (E2_analytical(i, i_state))**2
+        E2_analytical = DOT_PRODUCT(Avec_box(:,i_state), MATMUL(matrix_x2,Avec_box(:,i)))
+        WRITE(*,15), i, "-th state energy: ", Aval_box(i), " <", i_state,"| X^2 |Avec(",i,")> = ",  E2_analytical
         !
-        Total_E2(i_state) = Total_E2(i_state) + (E2_analytical(i, i_state))**2
      ENDDO
      !
-     CLOSE(UNIT = 79)
-     !
-     WRITE(*,*), "Total E2: ", i_state, Total_E2(i_state)
      !
   ENDDO
   !
@@ -154,11 +160,11 @@ SUBROUTINE E2_ISQW(Iprint, I_toten)
   OPEN(UNIT = 76, FILE = filename_TM, STATUS = "UNKNOWN", ACTION = "WRITE")
   !
   WRITE(76,*) "# BOX  dim_BOX = ", dim_BOX, " Box radius = ", X_max, " fm"
-  WRITE(76,*) "#Aval_box(i)    E2_analytical"
+  WRITE(76,*) "#Aval_box(i)    E2_numerical"
   !
   DO i = 1, dim_BOX     
      ! SAVING E2
-     WRITE(76,12)  Aval_box(i), E2_analytical(i,1:I_toten)
+     WRITE(76,12)  Aval_box(i), E2_numerical(i,1:I_toten)
      !
   ENDDO
   !
@@ -166,7 +172,7 @@ SUBROUTINE E2_ISQW(Iprint, I_toten)
   !
 11 FORMAT (1X,E16.8,1X,E17.8)
 12 FORMAT (1X,E16.8,1X,10E17.8) !!!! Take care of the number of bound states I_toten
-15 FORMAT (2X,I3,A,E16.8,2X,A,I2,A,E17.8)
+15 FORMAT (2X,I3,A,E16.8,2X,A,I3,A,I3,A,E17.8)
   !
   DEALLOCATE(matrix_x2, STAT = Ierr)
   IF (Ierr /= 0) THEN
@@ -187,9 +193,9 @@ SUBROUTINE E2_ISQW(Iprint, I_toten)
      STOP
   ENDIF
   !
-  DEALLOCATE(E2_analytical, STAT = Ierr)
+  DEALLOCATE(E2_numerical, STAT = Ierr)
   IF (Ierr /= 0) THEN
-     WRITE(*,*), "E2_analytical deallocation request denied."
+     WRITE(*,*), "E2_numerical deallocation request denied."
      STOP
   ENDIF
   !
